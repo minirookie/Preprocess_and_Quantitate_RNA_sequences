3402fb2e6296292c0eb3e96dbf30802779942ab5
Shanshan Bradford <87511458+minirookie@users.noreply.github.com>
2022-12-27 19:52:12 -0600

    Create Step1_SampleQC.sh

---
+++ Step1_SampleQC.sh
@@ -0,0 +1,29 @@
+## 1.1. Trim adaptors via HPC
+#BSUB -P RNASeq_pipeline
+#BSUB -n 8
+#BSUB -R "rusage[mem=4000]"
+#BSUB -R "span[hosts=1]"
+#BSUB -oo Jobreport_xxx_TrimAdaptor.out -eo Job_xxx_TrimAdaptor.err
+#BSUB -J QConSamples
+#BSUB -q standard
+
+fastqc=/research_jude/rgs01_jude/applications/hpcf/apps/fastqc/install/0.11.5/fastqc
+cutadapt=/research_jude/rgs01_jude/applications/hpcf/apps/python/install/3.6.1/bin/cutadapt
+
+FQfilepath=/research_jude/rgs01_jude/groups/yu3grp/projects/RelapseALL/yu3grp/AML/JefferyKlco/SELHEM_RNASeq/BAMprocessing_SYB/FASTQ_files_SeTrial
+QCoutputPath=/research_jude/rgs01_jude/groups/yu3grp/projects/RelapseALL/yu3grp/AML/JefferyKlco/SELHEM_RNASeq/BAMprocessing_SYB/FASTQ_files_SeTrial/FASTQC_output_cleanFiles
+
+module load python/3.6.1
+$cutadapt -a AGATCGGAAGAG -A AGATCGGAAGAG --trim-n --max-n=0.5 --quality-base=33 -q 30,20 -m 30 -o $FQfilepath/xxx_PE1.clean.fq.gz -p $FQfilepath/xxx_PE2.clean.fq.gz $FQfilepath/xxx_PE1.raw.fq.gz $FQfilepath/xxx_PE2.raw.fq.gz
+
+$fastqc $FQfilepath/xxx_PE1.clean.fq.gz -o $QCoutputPath 
+$fastqc $FQfilepath/xxx_PE2.clean.fq.gz -o $QCoutputPath
+
+
+FQfilepath=/research_jude/rgs01_jude/groups/yu3grp/projects/RelapseALL/yu3grp/AML/JefferyKlco/SELHEM_RNASeq/BAMprocessing_SYB/FASTQ_files_SeTrial
+for f in $FQfilepath/*.raw.fq.gz; do
+name=$(basename $f .raw.fq.gz)
+echo ${name/%_PE*/}
+cat $FQfilepath/FASTQC_output_cleanFiles/QConFQ_SeTrial_HPCJob.sh | sed -e 's/xxx/'${name/%_PE*/}'/g' > tmp
+bsub < tmp
+done
